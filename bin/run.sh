ede1ba70f9b539b2866187b97bd373ca0204c6d1
xiangfeng <xuxf5@yonghui.cn>
2023-03-04 13:39:25 +0800

    新增期货背驰筛选

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,3 @@
 cd /data/Projects/shop_distance
 
-nohup python3 -m src.get_basis_info --path /data/Projects/shop_distance > /data/Projects/shop_distance/aa.log 2>&1 &
-
-nohup python3 -m src.ample_temp.py --period $1 --path /data/Projects/shop_distance > /data/Projects/shop_distance/aa2.log 2>&1 &
+nohup python3 -m src.ample_temp --period $1 --path /data/Projects/shop_distance > /data/Projects/shop_distance/bin/aa2.log 2>&1 &
---
+++ bin/run_step_one.sh
@@ -0,0 +1,3 @@
+cd /data/Projects/shop_distance
+
+nohup python3 -m src.get_basis_info --path /data/Projects/shop_distance > /data/Projects/shop_distance/bin/aa.log 2>&1 &
